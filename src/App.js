6d839466a7297231e3c238bd2fbd28e69eb54b2c
G-ZUCK <tarifahuillca9706@gmail.com>
2023-08-04 11:48:05 -0500

    version 22

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import C01componente from './componente/C01componente';
-import Contador from './componente/C02contador';
-import Contador2 from './componente/C03dobleestado';
-import Variable from './componente/C04variable';
-import Operador from './componente/C05operadorTernario';
 import Matriz from './componente/C06matriz1';
+import Variable from './componente/C04variable';
+import Contador from './componente/C02contador';
 
 function App() {
   return (
     <div >
-      <h1>Primer componente</h1>
-      <C01componente/>
-      <h1>Segundo componente</h1>
-      <Contador/>
-      <h1>Tercer componente</h1>
-      <Contador2/>
-      <h1>Cuarto componente</h1>
-      <Variable/>
-      <h1>Quinto componente</h1>
-      <Operador/>
       <h1>Sexto componente</h1>
       <Matriz/>
-      
-      
+      <h1>Cuarto componente</h1>
+      <Variable/>
+      <h1>Segundo componente</h1>
+      <Contador/>
       
     </div>
   );
