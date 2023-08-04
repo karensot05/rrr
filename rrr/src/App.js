f9244596156ac47a6eccdbc4f62c4210ef2e9560
karensot05 <137527503+karensot05@users.noreply.github.com>
2023-08-04 15:04:22 -0500

    Add files via upload

---
+++ rrr/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Matriz from './componente/C06matriz1';
+import Variable from './componente/C04variable';
+import Contador from './componente/C02contador';
+
+function App() {
+  return (
+    <div >
+      <h1>Sexto componente</h1>
+      <Matriz/>
+      <h1>Cuarto componente</h1>
+      <Variable/>
+      <h1>Segundo componente</h1>
+      <Contador/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ rrr/src/componente/C01componente.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Componente = () => {
+  return ( <h1>Componente (JSX)</h1> );
+    <div>
+    Saludo desde react  
+    </div>
+  
+}
+
+export default Componente
